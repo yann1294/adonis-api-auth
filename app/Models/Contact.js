73efc6924592afce6c1cc7bc306bb308637837cf
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-09-04 10:08:28 +0530

    Contact table

--- app/Models/Contact.js
+++ app/Models/Contact.js
@@ -1,12 +1,9 @@
 "use strict";
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
-
 class Contact extends Model {
   user() {
     return this.belongsTo("App/Models/User");
   }
 }
-
 module.exports = Contact;
