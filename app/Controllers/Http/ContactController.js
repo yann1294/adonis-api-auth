742560f640a0b8f0f6fa46a13724380d6c853f69
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-09-04 19:06:44 +0530

    update

--- app/Controllers/Http/ContactController.js
+++ app/Controllers/Http/ContactController.js
@@ -3,7 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const Contact = use("App/Models/Contact");
 /**
  * Resourceful controller for interacting with contacts
  */
--- app/Models/Contact.js
+++ app/Models/Contact.js
@@ -1,7 +1,6 @@
 "use strict";
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
-const Contact = use("App/Models/Contact");
 class Contact extends Model {
   user() {
     return this.belongsTo("App/Models/User");
